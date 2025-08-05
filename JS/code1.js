02a5d7ff3ad84367c190f3f73e977d7fdc22c738
unknown <gdutt2304@gmail.com>
2025-08-05 19:46:57 +0530

    Udpate DataTye

---
+++ JS/code1.js
@@ -0,0 +1,35 @@
+// var a = 67
+// console.log(a)
+
+// var a = 78
+// console.log(a);
+
+
+// let nameValue  = "Ganesh"
+// let nameValue = "Dutt"
+
+// const value  = 45
+// console.log(value);
+
+
+
+
+
+// var value  = 23;
+// var value = 78;
+
+// value= 90; 
+// console.log(value);
+
+
+let a = 78
+// let a = 78 ❌
+a = 90
+console.log(a);
+
+const constValue = 90
+// constValue= 98   ❌
+console.log(constValue);
+
+
+
---
+++ JS/code2.js
@@ -0,0 +1,57 @@
+console.log("Class_2");
+
+// let a = "Ganesh Dutt";
+// let a = "Ganesh Dutt";
+// a = "Ganesh Sharma"
+// console.log(a);
+
+// var a = "Ganesh "
+
+// var a  = "Ganesh Dutt"
+// a ="Ganesh Sharma "
+// console.log(a);
+
+
+// const a = 8
+// a = "Ganesh "
+// console.log(a);
+
+// const a = 9 ;
+   
+// let a = 78
+// let a = 78.7
+// let a = "784738"
+// let a = '784738'
+// let a;
+// let a  = 48384837843848374873848374843784783787n
+
+// let a  = true;
+// let a  = false;
+
+// console.log(a);
+
+// Object dataType 
+
+// array 
+// let students  = ['a','b', 'c','d']
+// console.log(students);
+
+// Object 
+// let name  = "xyz"
+// let age  = 34
+// let course  = 'fs'
+
+// let person  = {
+//     // key : value 
+//     name : "xyz",
+//     age :34,
+//     course:"fs"
+// }
+// console.log(person.name);
+
+
+// let currentDate  = new Date()
+// console.log(currentDate);
+
+
+
