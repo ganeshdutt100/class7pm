602a5b05d74611798ceb69fbaca92491afe2902b
unknown <gdutt2304@gmail.com>
2025-08-05 20:06:56 +0530

    Update op

---
+++ JS/code3.js
@@ -0,0 +1,24 @@
+console.log("Code_3");
+
+// let b = 2;
+
+// let value = a + b;
+// let value = a - b;
+// let value = a * b;
+// let value = a / b;
+// let value = a % b;
+// a++
+// ++a
+// a--
+// --a
+
+// let a = 8;
+
+// console.log(a++); 
+// console.log(++a);
+// console.log(a--);
+
+// console.log(a++);
+
+// console.log(a);
+
