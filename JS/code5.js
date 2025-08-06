dce2fd59345c972ea54c75f31d907147d2a4f949
unknown <gdutt2304@gmail.com>
2025-08-06 19:46:55 +0530

    Udpate If ,if else,  if else if

---
+++ JS/code5.js
@@ -0,0 +1,31 @@
+// let a = 11 ;
+// if(a % 2 == 0 ){
+//     console.log("Even");
+    
+// }
+
+// if(a % 2 == 0 ){
+//     console.log("Even");
+    
+// } else{
+//     console.log("Odd");
+    
+// }
+
+// let marks  = 10;
+
+// if(marks >= 90){
+//     console.log("Grade A");
+    
+// }
+// else if(marks >= 60){
+//     console.log("Grade B");
+    
+// }
+// else if(marks >= 40){
+//     console.log("Grade C");
+    
+// }else {
+//     console.log("Grade F");
+    
+// }
