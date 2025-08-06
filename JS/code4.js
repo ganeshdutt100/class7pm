1c23fb748eee8cc19f4490ed659f83b4379100b8
unknown <gdutt2304@gmail.com>
2025-08-06 19:30:39 +0530

    OP

---
+++ JS/code4.js
@@ -0,0 +1,52 @@
+console.log("Code_4");
+
+// let a = 6
+// let b = "6"
+
+// console.log(a == b); //value 
+// console.log(a === b); //dataType and value 
+// console.log(a > b);
+// console.log(a < b);
+// console.log(a <= b);
+// console.log(a >= b);
+// console.log((a != b));
+
+
+// let a = 8;
+//   a += 5 //a = a+5;
+//   a -= 5 
+//   a *= 5 
+//   a /= 5 
+// console.log(a);
+
+
+// let value  = 78;
+// // value  = value +5;
+// value +=5
+// console.log(value );
+
+
+// AND(&&) or(||)  not(!)
+
+// let a = 6
+// let b = 8;
+
+// console.log();
+// true && false  = false
+// true && true = true
+// false && true = false
+// false && false = false
+
+
+// true  || false  = true
+// true  || true = true
+// false ||  true = true
+// false ||  false = false
+// !(true  == true )
+
+
+// let value = 4;
+
+// // (condition)? true : false 
+// let output  = (value % 2 == 0)?"Even":"odd"
+// console.log(output);
