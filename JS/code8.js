34e4851b921d847f3d304278854b12c9d21a9b89
unknown <gdutt2304@gmail.com>
2025-08-12 19:43:35 +0530

    Update for , while and do-while

--- JS/code8.js
+++ JS/code8.js
@@ -7,3 +7,26 @@
 //     console.log(i);
     
 // }
+
+// let table  = 13;
+// for(let i = 1; i <= 10; i++){
+//     console.log(table + " * " + i + " = " +  table * i);
+//     }
+
+// 2*1= 2
+
+// let i = 1;
+// while(i <= 5){
+//     console.log(i);
+//     i++
+    
+// }
+
+
+let i  = 11;
+
+do{
+    console.log(i);
+    i++;
+    
+}while(i <= 10)
