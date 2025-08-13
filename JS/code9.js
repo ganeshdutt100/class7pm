facbef4c2fdc15a2ff0b85849b386b1c34f340e9
unknown <gdutt2304@gmail.com>
2025-08-13 19:59:34 +0530

    Update of and in loop

---
+++ JS/code9.js
@@ -0,0 +1,28 @@
+let a = [12,45,78,98,65] // array
+// console.log(a);
+// for(let i = 0; i<= a.length; i++){
+//     console.log(a[i]);
+    
+// }
+
+// for(let x  of a){
+//     console.log(x);
+    
+// }
+
+// let name  = "Ganesh Dutt"
+// let course = 'fs'
+// let age   = 25
+// let address = Mathura
+
+// let person = {
+//     name: "Ganesh Dutt",
+//     age: 25,
+//     course : 'fs',
+//     address :"Mathura"
+// }
+// console.log(person);
+// for(let value in person){
+//     console.log(value + " : "  +person[value]);
+    
+// }
