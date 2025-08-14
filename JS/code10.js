0fa6f04c49790a5bf868d88e6f48abe6c2dd7fe6
unknown <gdutt2304@gmail.com>
2025-08-14 20:03:57 +0530

    Update Array Questions

---
+++ JS/code10.js
@@ -0,0 +1,53 @@
+console.log("Hello World");
+
+// let arr = [1,2,3,4,5,6,7,8,9,10]
+// let  sum  = 0 ;
+
+// console.log(arr);
+// for(let x   of arr){
+//     // sum = sum + x
+//     sum+=x
+    
+// }
+// console.log(sum);
+
+
+// Create an array of 5 numbers and print the first and last elements.
+// console.log(arr[0]);
+// console.log(arr[4]);
+
+
+// Add a new element to the end of an array without using a method.
+// let arr = [1,2,3,4,5,6,7,8,9,10]
+// let element  = 12;
+// arr[arr.length] = element;
+// console.log(arr);
+
+//   arr.push(12)
+// console.log(arr);
+
+
+// Remove the first element from an array.
+// let arr = [1,2,3,4,5,6,7,8,9,10]
+// for(let i = 0; i < arr.length-1; i++){
+//     arr[i]= arr[i+1]
+//     console.log(arr[i]);
+    
+    
+// }
+// arr[0]= arr[1]
+// arr[1]= arr[2]
+// arr[2]= arr[3]
+// arr[3]= arr[4]
+// arr[4]= arr[5]
+
+// Find the length of an array.
+let arr = [12,45,67,87,65,34]
+let count = 0
+while(arr[count] !== undefined){
+    count++;
+}
+console.log(count);
+// console.log(arr.length);
+
+
