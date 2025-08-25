9d694e76f4b111d482367e2039e0f613b1910ea0
unknown <gdutt2304@gmail.com>
2025-08-25 19:24:25 +0530

    Accessor Methods

--- JS/arrMethods.js
+++ JS/arrMethods.js
@@ -1,4 +1,4 @@
-let arrNum  = [12,34,25,27,8 ,1]
+// let arrNum  = [12,34,25,27,8 ,1]
 // fill
 // arrNum.fill("JS",2,5)
 
@@ -23,8 +23,24 @@ let arrNum  = [12,34,25,27,8 ,1]
 // splice
 // splice(start , delete... , item1..)
 
-arrNum.splice(4,2 , "JS","CSS")
+// arrNum.splice(4,2 , "JS","CSS")
 
 // reverse
 // arrNum.reverse()
-console.log(arrNum);
+// console.log(arrNum);
+
+
+
+let arrNum1  = [12,34,25,27,8 ,1,12]
+// let arrNum2  = [120,340,215,227,82 ,13]
+
+// let result = arrNum1.concat(arrNum2)
+// let result = arrNum1.includes(1)
+// let result = arrNum1.indexOf(1)
+// let result = arrNum1.lastIndexOf(12)
+// let result = arrNum1.slice(1 , 3)
+// let result = arrNum1.toString()
+// let result = arrNum1.join(" /  ")
+console.log(result);
+
+
