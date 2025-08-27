b4609c443e254204b86a1c497c26919364f8be57
unknown <gdutt2304@gmail.com>
2025-08-27 19:21:00 +0530

    Update object

---
+++ JS/obj1.js
@@ -0,0 +1,11 @@
+let person = {
+    name : 'js',
+    age : 20,
+    job : 'developer'
+
+}
+// console.log(person.name  , person.age);
+for(let x in person){
+    console.log(x  , " :  ", person[x]);
+    
+}
